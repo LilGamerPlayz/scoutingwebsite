d7bbe9fcd0e8334f6ab3a235adf4a42893a3387a
Erick Tran <thelittlebotengineer@gmail.com>
2023-01-31 10:29:54 -0800

    Update 1.31.23.4b

--- public/js-css/Scouting.js
+++
@@ -1,8 +0,0 @@
-function MatchScouting() {
-    localStorage.setItem("ScoutingType", "Match");
-    window.location = "Match-Scouting";
-}
-function PitScouting() {
-    localStorage.setItem("ScoutingType", "Pit");
-    window.location = "Pit-Scouting";
-}
--- public/js-css/home.js
+++ public/js-css/home.js
@@ -1,9 +1,11 @@
-function Notes() {
-    window.location = "Notes";
-}
 function ScoutingType() {
     window.location = "ScoutingType";
+    if (!localStorage.getItem("Name")) {
+        let name = prompt("Please enter your name", "Name");
+        localStorage.setItem("Name", name);
+    }
 }
+
 function Survey() {
     window.location = "Survey";
 }
