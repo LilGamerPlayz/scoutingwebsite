9102922e402bc5430bc065ffd1d42ee8daaad4d1
potttaaatttooo <nathannguyen202@gmail.com>
2023-03-01 16:04:19 -0800

    Update 3.1.23.1a

--- public/js-css/Submit.js
+++ public/js-css/Submit.js
@@ -12,43 +12,69 @@ async function Submit() {
 
 
     Yes.addEventListener("click", async function () {
-        let Title = document.getElementById("Title").textContent;
-
         if (localStorage.getItem("MatchType") == "Pit-Scouting") {
-            
-        }
-
-        data.push({
-            MatchType: localStorage.getItem("MatchType"),
-            TeamNumber: localStorage.getItem("TeamNumber"),
-            GamePieces: localStorage.getItem("GamePieces"),
-            DriverExperience: localStorage.getItem("DriverExperience"),
-            DriveTrain: localStorage.getItem("DriveTrain"),
-            AutoPlan: localStorage.getItem("AutoPlan"),
-            GamePieceLocation: localStorage.getItem("GamePieceLocation"),
-            AverageTime: localStorage.getItem("AverageTime"),
-            ScoringLocation: localStorage.getItem("ScoringLocation"),
-            DockEngage: localStorage.getItem("DockEngage"),
-            PictureRobot: localStorage.getItem("PictureRobot"),
-            Comments: localStorage.getItem("Comments")
-        });
-
-        let scouting = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data),
-        }
-    
-        await fetch("/scouting", scouting)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
+            data.push({
+                MatchType: localStorage.getItem("MatchType"),
+                TeamNumber: localStorage.getItem("TeamNum"),
+                DriverExperience: localStorage.getItem("DriverExperience"),
+                DriveTrain: localStorage.getItem("DriveTrain"),
+                AutoPlan: localStorage.getItem("AutoPlan"),
+                GamePieceLocation: localStorage.getItem("GamePieceLocation"),
+                AverageTime: localStorage.getItem("AverageTime"),
+                ScoringLocation: localStorage.getItem("ScoringLocation"),
+                DockEngage: localStorage.getItem("DockEngage"),
+                PictureRobot: localStorage.getItem("PictureRobot"),
+                Comments: localStorage.getItem("Comments")
             });
-    });
 
-    No.addEventListener("click", function () {
-        document.getElementById("SubmitBox").style.display = "none";
+            let scouting = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data),
+            }
+
+            await fetch("/scouting", scouting)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                });
+
+        window.location = "Submitted";
+        } else if (localStorage.getItem("MatchType") == "Match-Scouting") {
+            data.push({
+                MatchType: localStorage.getItem("MatchType"),
+                TeamNumber: localStorage.getItem("TeamNumber"),
+                GamePieces: localStorage.getItem("GamePieces"),
+                DriverExperience: localStorage.getItem("DriverExperience"),
+                DriveTrain: localStorage.getItem("DriveTrain"),
+                AutoPlan: localStorage.getItem("AutoPlan"),
+                GamePieceLocation: localStorage.getItem("GamePieceLocation"),
+                AverageTime: localStorage.getItem("AverageTime"),
+                ScoringLocation: localStorage.getItem("ScoringLocation"),
+                DockEngage: localStorage.getItem("DockEngage"),
+                PictureRobot: localStorage.getItem("PictureRobot"),
+                Comments: localStorage.getItem("Comments")
+            });
+
+            let scouting = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data),
+            }
+
+            await fetch("/scouting", scouting)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                });
+        }
+
+        No.addEventListener("click", function () {
+            document.getElementById("SubmitBox").style.display = "none";
+        });
     });
-}
+};
