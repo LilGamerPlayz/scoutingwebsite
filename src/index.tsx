ceaf1cbc63aae71f63c9c470c9bfdf806b2a9558
Erick Tran <thelittlebotengineer@gmail.com>
2024-04-07 16:47:05 -0700

    Update 4.7.2024.1a

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppRoutes from './AppRoutes.global';
+import './index.css';
+import App from './App';
 
-const root: any = ReactDOM.createRoot(
+const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <AppRoutes />
+    <App />
   </React.StrictMode>
 );
